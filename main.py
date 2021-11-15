c0cf003f3d0e31ed9a9092dc185a60b4c8d59955
DramaTourG <lema091992@gmail.com>
2021-11-16 00:31:27 +0100

    feat(db connection): added async connection to db

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from typing import Optional
 from fastapi import FastAPI
+from db.base import database
 import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 if __name__ == '__main__':
