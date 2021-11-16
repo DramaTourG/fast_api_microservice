1389e16a0635495bfee63dbf3aa2e4ebd7730216
DramaTourG <lema091992@gmail.com>
2021-11-16 02:26:02 +0100

    feat(table): added table Users to db

--- core/config.py
+++ core/config.py
@@ -2,4 +2,6 @@ from starlette.config import Config
 
 config = Config('.env')
 
-DATABASE_URL = config("EE_DATABASE_URL", cast=str, default="postgresql://Dev:hello@localhost:5432/development_db")
+DATABASE_URL = config(
+    "EE_DATABASE_URL", cast=str,
+    default="postgresql://Dev:hello@localhost:5432/development_db")
---
+++ db/users.py
@@ -0,0 +1,23 @@
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
+from sqlalchemy import Column, Table, Integer, String
+
+from db.base import metadata
+
+Users = Table('Users', metadata,
+              Column('id', Integer, primary_key=True),
+              Column('username', String(16), nullable=False),
+              Column('email', String(60), unique=True, nullable=False),
+              Column('password', String(500), nullable=False),
+              Column('register_date', String(50), nullable=False)
+              )
+
+
+class BookSchema(SQLAlchemySchema):
+    class Meta:
+        model = Users
+        load_instance = True
+
+    id = auto_field()
+    username = auto_field()
+    email = auto_field()
+    register_date = auto_field()
