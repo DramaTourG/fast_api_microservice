615245c96e789a4cdb4102c7313b4b83333068c9
DramaTourG <lema091992@gmail.com>
2021-11-18 23:45:07 +0100

    feat(token): added depends for validation

--- core/security.py
+++ core/security.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta
-
 import bcrypt
+from fastapi import Request, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import jwt
+
 
 from core.config import ACCESS_TOKEN_EXPIRE_HOURS, SECRET_KEY, ALGORITHM
 
@@ -27,3 +29,14 @@ def decode_access_token(token: str):
     except jwt.JWSError:
         return None
     return decoded_jwt
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        if credentials and decode_access_token(credentials.credentials):
+            return credentials.credentials
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid token")
--- endpoints/depends.py
+++ endpoints/depends.py
@@ -1,6 +1,26 @@
+from fastapi import Depends, HTTPException, status
+
+from models.user_models import UserIn
 from repositories.users_repo import UserRepo
 from db.base import database
+from core.security import JWTBearer, decode_access_token
 
 
 def get_user_repository() -> UserRepo:
     return UserRepo(database)
+
+
+async def get_current_user(
+        users: UserRepo = Depends(get_user_repository()),
+        token: str = Depends(JWTBearer)) -> UserIn:
+    credentials_exc = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Invalid credentials')
+    payload = decode_access_token(token)
+    if payload is None:
+        raise credentials_exc
+    email = payload.get("sub")
+    if email is None:
+        raise credentials_exc
+    user = await users.get_by_email(email)
+    if user is None:
+        raise credentials_exc
+    return user
