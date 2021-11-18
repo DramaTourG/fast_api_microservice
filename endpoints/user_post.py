aa4013592c1b0c3683850049dfabfea173509f3d
DramaTourG <lema091992@gmail.com>
2021-11-18 17:23:49 +0100

    feat(endpoint): added user post route

---
+++ endpoints/user_post.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Depends
+from repositories.users_repo import UserRepo
+from .depends import get_user_repository
+from models.user import UserOut, UserIn
+
+router = APIRouter()
+
+
+@router.post("/user", response_model=UserOut, status_code=201)
+async def create_user(user: UserIn, users: UserRepo = Depends(get_user_repository)):
+    return await users.create(user)
