b8f8552f85f27274bdde65fd32342d00cdc8cfef
DramaTourG <lema091992@gmail.com>
2021-11-18 22:38:36 +0100

    feat(test): added tests for creting and encoding token

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,6 +1,6 @@
 import pytest
 
-from core.security import verify_password, password_hashing
+from core.security import verify_password, password_hashing, create_access_token, decode_access_token
 
 
 def test_hashing_and_verify_psw():
@@ -10,3 +10,13 @@ def test_hashing_and_verify_psw():
     with pytest.raises(ValueError):
         verify_password(hashed_password, psw)
     assert verify_password('WrongPassword', hashed_password) is False
+
+
+def test_create_access_token():
+    data = {"sub": "test@email.com"}
+    token = create_access_token(data)
+    data2 = decode_access_token(token)
+
+    assert type(token) == str
+    assert data['sub'] == data2['sub']
+    assert data2['exp']
