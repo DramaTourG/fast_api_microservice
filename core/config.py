38132dffe9832870fe94771d58eee060f2b59ba2
DramaTourG <lema091992@gmail.com>
2021-11-19 20:35:21 +0100

    feat(migrations): added alembic migrations

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@ from starlette.config import Config
 config = Config('.env')
 
 DATABASE_URL = config(
-    "EE_DATABASE_URL", cast=str,
+    "DATABASE_URL", cast=str,
     default="postgresql://Dev:hello@localhost:5432/development_db")
 ACCESS_TOKEN_EXPIRE_HOURS = 20
 ALGORITHM = "HS256"
--- db/__init__.py
+++ db/__init__.py
@@ -1,3 +1,3 @@
 from .base import metadata, engine
 
-metadata.create_all(bind=engine)
+# metadata.create_all(bind=engine)
