0be41447cca57d7fcfaec9ca672a448f495648fb
DramaTourG <lema091992@gmail.com>
2021-11-19 22:41:13 +0100

    feat(config): added os env

--- core/config.py
+++ core/config.py
@@ -1,11 +1,10 @@
-from starlette.config import Config
+import os
+import dotenv
 
-config = Config('.env')
+dotenv.load_dotenv('.env')
 
-DATABASE_URL = config(
-    "DATABASE_URL", cast=str,
-    default="postgresql://Dev:hello@localhost:5432/development_db")
+DATABASE_URL = os.environ.get("DATABASE_URL","postgresql://Dev:hello@localhost:5432/development_db")
 ACCESS_TOKEN_EXPIRE_HOURS = 20
 ALGORITHM = "HS256"
-SECRET_KEY = config("SECRET_KEY", cast=str, default="N0tASecretKey")
-ORIGINS = config("SECRET_KEY", cast=list, default=["http://localhost", "http://localhost:8080"])
+SECRET_KEY = os.environ.get("SECRET_KEY", "N0tASecretKey")
+ORIGINS = os.environ.get("ORIGINS", ["http://localhost", "http://localhost:8080"])
